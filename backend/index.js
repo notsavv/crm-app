e3cc978c74221621c8b111e2090e05a54fda68fb
Dimitris Savvidis <dimitrissavvidis@outlook.com>
2025-02-05 14:28:38 +0200

    crm-building-note #1

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
 
-dotenv.config()
+dotenv.config({ path: '../.env' })
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-
+console.log(process.env.PORT)
 app.get('/', (req,res)=>{
     res.send('server is running')
 })
